9e2aa53acc9f60c066fb7da72dff9ea0cb482de8
KAanti-VP <144044786+KAanti-VP@users.noreply.github.com>
2023-09-04 18:23:14 +0200

    Add files via upload
    task A

---
+++ AVariableModifications.java
@@ -0,0 +1,49 @@
+public class AVariableModifications {
+    public static void main(String[] args) {
+        int a = 3;
+        // increase the value of variable "a" by 10 and print the new value
+
+
+        int b = 100;
+        // decrease the value of b by 7 and print the new value
+
+
+        int c = 44;
+        // double the value of c and print the new value
+
+
+        int d = 125;
+        // divide the value of d by 5 and print the new value
+
+
+        int e = 8;
+        // cube the value of e and print the new value
+
+
+        int f1 = 123;
+        int f2 = 345;
+        // determine if f1 is greater than f2 (print as a boolean)
+
+
+        int g1 = 350;
+        int g2 = 200;
+        // determine if double the value of g2 is greater
+        // than g1 (print as a boolean)
+
+
+        int h = 135798745;
+        // determine if h has 11 as a divisor (print as a boolean)
+
+
+        int i1 = 10;
+        int i2 = 3;
+        // determine if i1 is greater than i2 squared and smaller than i2 cubed
+        // (print as a boolean)
+
+
+        int j = 1521;
+        // determine if j is divisible by 3 or 5 (print as a boolean)
+
+
+    }
+}
